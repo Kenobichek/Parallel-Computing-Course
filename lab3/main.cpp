a536a3cd54840da8c72fff0ce90af962ef1d3715
Kenobichek <lesechklesia@gmail.com>
2023-04-06 16:40:42 +0300

    Completed task according to the variant using blocking primitives synchronization.

--- lab3/main.cpp
+++ lab3/main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 #include <vector>
 #include <random>
+#include <chrono>
+#include <mutex>
+#include <thread>
 
 std::vector<int> generateRandomVector(const int sizeArray)
 {
@@ -20,22 +23,71 @@ std::vector<int> generateRandomVector(const int sizeArray)
     return vec;
 }
 
-int sumOfOddMod2(const std::vector<int>& vec) 
+
+std::mutex mtx;
+
+int sumOfOddMod2(const std::vector<int>& vec, int start, int end) 
 {
     int sum = 0;
-    for (const auto& elem : vec) 
+
+    for (int i = start; i < end; i++) 
     {
-        if (elem % 2 == 1) 
+        if (vec[i] % 2 != 0) 
         {
-            sum += elem;
+            mtx.lock();
+            sum += vec[i];
+            mtx.unlock();
         }
     }
+
     return sum;
+}
+
+void task(const std::vector<int>& vec, int numThreads)
+{
+	std::vector<std::thread> threads(numThreads);
+	int numPerThread = vec.size() / numThreads;
+	int remaining = vec.size() % numThreads;
+	int start = 0;
+
+	for(int i = 0; i < numThreads; i++)
+	{
+		int end = start + numPerThread;
+
+		if (i < remaining)
+		{
+			end++;
+		}
+
+		threads[i] = std::thread(&sumOfOddMod2, std::ref(vec), start, end);
+	}
+
+	for (auto& thread : threads)
+	{
+		thread.join();
+	}
 }
 
 int main()
 {
-	int sizeArray = 100;
-    std::vector<int> vec = generateRandomVector(sizeArray);
-	std::cout << sumOfOddMod2(vec);
+	std::vector<int> vectorSizes = { 50, 100, 500, 5000, 10000 };
+    std::vector<int> numThreadsSizes = { 1, 2, 3, 4 };
+
+
+	for(auto size : vectorSizes)
+	{
+        std::vector<int> vec = generateRandomVector(size);
+
+        for (auto threads : numThreadsSizes)
+        {
+            auto startTime = std::chrono::high_resolution_clock::now();
+            task(vec, threads);
+            auto endTime = std::chrono::high_resolution_clock::now(); 
+
+            std::chrono::duration<double, std::milli> elapsed_time = endTime - startTime;
+            std::cout << "Elapsed time: " << elapsed_time.count() << " ms" << std::endl;
+        }
+	}
+
+    return 0;
 }
